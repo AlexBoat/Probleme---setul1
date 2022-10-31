297ab2aa88ce788a0a2250757d29c1925f184ec0
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-10-31 17:13:46 +0200

    Add files via upload

---
+++ Ex_3/Ex_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //n se divide cu k
+            int n, k;
+            n = Convert.ToInt32(Console.ReadLine());
+            k = Convert.ToInt32(Console.ReadLine());
+            if(n%k==0)
+            {
+                Console.WriteLine("{0} se divide cu {1}", n, k);
+            }
+            else
+                Console.WriteLine("{0} nu se divide cu {1}", n, k);
+        }
+    }
+}
