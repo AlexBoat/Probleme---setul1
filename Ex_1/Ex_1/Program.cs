715959837d952b85b66db929e0d7326071089118
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-10-31 16:33:10 +0200

    Add files via upload

---
+++ Ex_1/Ex_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ax+b=0
+            float a, b, x;
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            x = -b / a;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
