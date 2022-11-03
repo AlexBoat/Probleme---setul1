5c1cb368f35bc83565cdf829994cdf46393bbaf5
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-11-03 08:57:20 +0200

    Add files via upload

---
+++ Ex_7/Ex_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, aux;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            aux = a;
+            a = b;
+            b = aux;
+            Console.WriteLine("{0} {1}", a, b);
+        }
+    }
+}
