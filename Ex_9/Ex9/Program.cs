d84ffcf54860f961984a2dc5086559c8ae460d43
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-11-03 09:04:15 +0200

    Add files via upload

---
+++ Ex_9/Ex9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=n; i++)
+            {
+                if(n%i==0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
