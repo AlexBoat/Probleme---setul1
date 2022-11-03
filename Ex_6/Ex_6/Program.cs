561930292503b251726710ba7e97a03863784364
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-11-03 08:48:41 +0200

    Add files via upload

---
+++ Ex_6/Ex_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > 0 && b > 0 && c > 0 && (a + b > c || a + c > b || b + c > a))
+            {
+                Console.WriteLine("{0} {1} {2} pot forma un triunghi", a, b, c);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1} {2} nu pot forma un triunghi", a, b, c);
+            }
+        }
+    }
+}
