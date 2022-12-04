248791202fcfffa37cff328127932c5a527071f6
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:22:05 +0200

    Add files via upload

---
+++ Ex_11/Ex_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, ogl=0;
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n!=0)
+            {
+                ogl = ogl * 10 + n % 10;
+                n /= 10;
+            }
+            Console.WriteLine(ogl);
+        }
+    }
+}
---
+++ Ex_12/Ex_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, a, b, nr=0;
+            n = Convert.ToInt32(Console.ReadLine());
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            for(int i=a; i<=b; i++)
+            {
+                if(i%n==0)
+                {
+                    nr++;
+                }
+            }
+            Console.WriteLine(nr);
+        }
+    }
+}
