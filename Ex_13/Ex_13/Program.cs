fe68b1e4560b8e155e330b11673884e960e314d5
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:25:40 +0200

    Add files via upload

---
+++ Ex_13/Ex_13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static bool bisect(int an)
+        {
+            if (an % 400 == 0)
+            {
+                return true;
+            }
+            if (an % 100 == 0)
+            {
+                return false;
+            }
+            if (an % 4 == 0)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        public static void Main(string[] args)
+        {
+            int y1, y2;
+            int nr=0;
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            for(int i=y1; i<=y2; i++)
+            {
+                if(bisect(i)==true)
+                {
+                    nr++;
+                }
+            }
+            Console.WriteLine(nr);
+        }
+    }
+}
---
+++ Ex_14/Ex_14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, aux, ogl = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            aux = n;
+            while(aux!=0)
+            {
+                ogl = ogl * 10 + aux % 10;
+                aux /=10;
+            }
+            if(n==ogl)
+            {
+                Console.WriteLine("{0} este palindrom", n);
+            }
+            else
+                Console.WriteLine("{0} nu este palindrom", n);
+
+        }
+    }
+}
