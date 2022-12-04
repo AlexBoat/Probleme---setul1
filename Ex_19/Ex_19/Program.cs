142bf7ee34156133804bd2afa7c6294f97cf8a3f
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:28:11 +0200

    Add files via upload

---
+++ Ex_19/Ex_19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Dynamic;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, c1=0, c2=0, aux;
+            bool ok = false;
+            n = Convert.ToInt32(Console.ReadLine());
+            aux = n;
+            c1 = aux % 10;
+            c2 = c1;
+            aux /= 10;
+            while(aux!=0)
+            {
+                if(aux%10!=c1 && ok==false)
+                {
+                    c2 = aux % 10;
+                    ok = true;
+                }
+                if(aux%10!=c1 && aux%10 != c2 && ok == true)
+                {
+                    ok = false;
+                    break;
+                }
+                aux /= 10;
+            }
+            if(ok==false)
+                Console.WriteLine("{0} nu este format doar din 2 cifre ce se pot repeta", n);
+            else
+                Console.WriteLine("{0} este format doar din 2 cifre ce se pot repeta", n);
+        }
+    }
+}
---
+++ Ex_20/Ex_20/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static bool isprim(int n)
+        {
+            for (int d = 2; d * d <= n; d++)
+                if (n % d == 0)
+                    return false;
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            bool ok = false;
+            float m, n;
+            m = float.Parse(Console.ReadLine());
+            n = float.Parse(Console.ReadLine());
+            //periodica mixta
+            
+            for(int i=3; i<=n; i++)
+            {
+                if(isprim(i))
+                {
+                    if((n%2==0 || n%5==0) && n%i==0)
+                    {
+                        //Console.WriteLine(m / n);
+                        Console.Write("Ioi nu, fractie periodica mixta...");
+                        ok = true;
+                        break;
+                    }
+
+                }
+
+            }
+            //periodica simpla
+            if(ok==false)
+            {
+                if(n % 2 != 0 && n % 5 != 0)
+                {
+                    Console.Write(Convert.ToInt32(m / n));
+                    Console.Write(".(");
+                    if(n<m)
+                        Console.Write(n%m + ")");
+                    else
+                        Console.Write(m%n + ")");
+
+                }
+                //neperiodica
+                else
+                    Console.WriteLine(m / n);
+
+            }
+            
+        }
+    }
+}
