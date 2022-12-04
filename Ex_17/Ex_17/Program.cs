e9dd01c2e572789e68513084be2033c07dfd21ae
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:27:23 +0200

    Add files via upload

---
+++ Ex_17/Ex_17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, n, m;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            n = a;
+            m = b;
+            while (n != m)
+                if (n > m)
+                    n -= m;
+                else
+                    m -= n;
+            Console.WriteLine("Cmmdc: {0}",n);
+            n = a;
+            m = b;
+            while (n != m)
+            {
+                if (n < m)
+                    n = n + a;
+                else
+                    m = m + b;
+            }
+            Console.WriteLine("Cmmmc: {0}", n);
+
+        }
+    }
+}
---
+++ Ex_18/Ex_18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int d = 2, p;      
+            while (n > 1)
+            {
+                p = 0;
+                while (n % d == 0)
+                {
+                    ++p;
+                    n /= d;
+                }
+                if (p!=0)
+                    Console.WriteLine("{0} {1}", d, p);
+                ++d;
+                if (n > 1 && d * d > n)
+                {
+                    d = n;
+                }
+            }
+        }
+    }
+}
