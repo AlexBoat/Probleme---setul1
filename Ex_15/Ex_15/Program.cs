b99975a43198dcb47052479f6594aaa3ff2b58b4
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:26:32 +0200

    Add files via upload

---
+++ Ex_15/Ex_15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[4];
+            for(int i=1; i<=3; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            bool sortat;
+            do
+            {
+                sortat = true;
+                for (int i = 1; i < 3; i++)
+                    if (a[i] > a[i + 1])
+                    {
+                        a[i] = a[i] + a[i + 1];
+                        a[i + 1] = a[i] - a[i + 1];
+                        a[i] = a[i] - a[i + 1];
+                        sortat = false;
+                    }
+            }
+            while (!sortat);
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
---
+++ Ex_16/Ex_16/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Standard
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            e = Convert.ToInt32(Console.ReadLine());
+            if(a>b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            if(c>d)
+            {
+                c = c + d;
+                d = c - d;
+                c = c - d;
+            }
+            if(b>d)
+            {
+                b = b + d;
+                d = b - d;
+                b = b - d;
+            }
+            if(c>e)
+            {
+                c = c + e;
+                e = c - e;
+                c = c - e;
+            }
+            if(b>e)
+            {
+                b = b + e;
+                e = b - e;
+                b = b - e;
+            }
+            if (a > c)
+            {
+                a = a + c;
+                c = a - c;
+                a = a - c;
+            }
+            if(b>c)
+            {
+                b = b + c;
+                c = b - c;
+                b = b - c;
+            }
+            if(d>e)
+            {
+                d = d + e;
+                e = d - e;
+                d = d - e;
+            }
+            if (c > d)
+            {
+                c = c + d;
+                d = c - d;
+                c = c - d;
+            }
+            Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+        }
+    }
+}
